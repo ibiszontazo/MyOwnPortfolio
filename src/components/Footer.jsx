5bcc7241f4b04aa352181d8e02da69a01697608b
dominikawilczynska <dm.wilczynska@student.uj.edu.pl>
2025-08-09 19:56:17 +0200

    Initial commit of dist for GitHub Pages

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-            <p>© {new Date().getFullYear()} Filip Mudyna. Wszelkie prawa zastrzeżone.</p>
-        </footer>
-    );
-}
-
-export default Footer;
--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('App')).render(
-    <App />
-)
-ReactDOM.render(<App />, document.querySelector(".App"));
