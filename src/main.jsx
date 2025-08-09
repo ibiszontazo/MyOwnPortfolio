36cb29336871909029139585ed628c9331e2ad8d
ibiszontazo <ibiszontazo@gmail.com>
2025-08-09 20:18:53 +0200

    fixing broken website

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
+import './styles/main.scss';
 
 
-const container = document.getElementById('App');
+const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
